e14bc0aa390d115d5482c731f7abfc2ffc515630
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2019-06-07 14:19:08 +1200

    Initial Commit

---
+++ Task1/Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            int count = CountDivisions(number);
+            Console.WriteLine($"Total number of divisions {count}");
+
+            Console.ReadKey();
+        }
+        public static int CountDivisions(double number)
+        {
+            int count = 0;
+
+            if(number > 0 && number % 2 == 0)
+            {
+                count++;
+                number /= 2;
+                return count += CountDivisions(number);
+            }
+            return count;
+        }
+    }
+}
