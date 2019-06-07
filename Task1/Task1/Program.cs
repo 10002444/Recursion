cf712c1d26eec8a537633c8a52063888d9870ae2
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2019-06-07 13:25:34 +1200

    Test commit

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+        }
+    }
+}
