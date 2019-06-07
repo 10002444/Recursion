db3e0882648f634ef3c16ac5bcaf60534f5334ac
Marisa Wadsworth <10002444@student.toiohomai.ac.nz>
2019-06-07 13:43:33 +1200

    Initial Commit

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -12,6 +12,24 @@ namespace Task1
         {
             Console.Write("Please enter the first number: ");
             int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = RecursivlyAdd(num1, num2);
+            Console.WriteLine($"The sum of the numbers form {num1} and {num2} is {sum}");
+
+        }
+        public static int RecursivlyAdd(int num1, int num2)
+        {
+            int sum = num1;
+
+            if(num1 < num2)
+            {
+                num1++;
+                return sum += RecursivlyAdd(num1, num2);
+            }
+            return sum;
         }
     }
 }
